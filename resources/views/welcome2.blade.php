c5122f82feb5061119084d7791e4b3920c829e05
Lopez <viridiana.lopez@DelphiDrive.com>
2022-09-29 16:38:30 -0500

    first commit

---
+++ resources/views/welcome2.blade.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+<div>
+    
+</div>
+    <h1 background-color="green" align="center" >Hoja de chequeo de cuarto limpio</h1>
+    <br>
+    <table class="table table-success table-bordered">
+        <thead>
+            <tr>
+                <th>No. de cambio</th>
+                <th>Fecha</th>
+                <th>ME/IE</th>
+                <th>T. Hernandez / C. Perez</th>
+                <th>Area</th>
+                <th>Cuarto Limpio 1</th>
+                <th>Lugar</th>
+                <th>Torreon</th>
+                <th>Pagina</th>
+                <th>1 de 2</th>
+            </tr>
+            <tr>
+                <th rowspan="2">1080636510</th>
+                <th>07-nov-17</th>
+                <th>calidad</th>
+                <th>R. Avitia. / B. Contreras</th>
+                <th>Linea</th>
+                <th>1</th>
+                <th>Planta</th>
+                <th colspan ="3">59</th>
+            </tr>
+            <tr>
+                <th>Rev. 01</th>
+                <th>Manufactura</th>
+                <th>R. Espinoza / H. Martinez / C. Lozano</th>
+                <th>Op #</th>
+                <th>20</th>
+                <th>Depto</th>
+                <th>926/970/971/959/972</th>
+                <th>No.Parte</th>
+                <th>Ver F2-FM-ELE-L1-01</th>
+            </tr>
+        </thead>
+    </table>
+    <div class="table-responsive">
+
+
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th colspan ="16">Cuarto Limpio 1</th>
+                <th colspan ="8">Dias del mes de: Octubre</th>
+                <th colspan ="8">Año: 2022</th>
+            </tr>
+            <tr>
+                <th rowspan = "2" >parametros turno</th>
+                <th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th><th>10</th>
+                <th>11</th><th>12</th><th>13</th><th>14</th><th>15</th><th>16</th><th>17</th><th>18</th><th>19</th>
+                <th>20</th><th>21</th><th>22</th><th>23</th><th>24</th><th>25</th><th>26</th><th>27</th><th>28</th>
+                <th>29</th><th>30</th><th>31</th>
+            </tr>
+            <tr>
+                <th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th>
+                <th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th>
+                <th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th>
+                <th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th><th>57</th>
+            </tr>
+            <tr>
+                <th colspan="32" class= "table-secondary"><p align ="center">CADA DIA (AL INICIO DEL 1ER TURNO)</p></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Humedad 50% +/-3%</td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td>
+            </tr>
+            <tr>
+                <td>Presion Minimo 0.08 In/Hg</td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td>
+            </tr>
+            <tr>
+                <td>Temperatura 7ºF +/- 2ºF</td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td>
+            </tr>
+            <tr>
+                <td>VERIFICACION DEL TECNICO</td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td>
+            </tr>
+            <tr>
+                <td>VERIFICACION DEL ENTRENADOR</td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td>
+            </tr>
+            <tr>
+                <td>VERIFICACION DEL AUDITOR DE CALIDAD</td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td><td><input type="checkbox"></td>
+                <td><input type="checkbox"></td>
+            </tr>
+            <tr>
+                <td colspan="32"><p>Nota 1: Especifique la lectura de los controles (KCC'S) al momento de realizar su revision</p></td>
+            </tr>
+            <tr>
+                <td colspan="32"><p>Nota 2: Documentar acciones correctivas atras del formato</p></td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+</body>
+</html>
