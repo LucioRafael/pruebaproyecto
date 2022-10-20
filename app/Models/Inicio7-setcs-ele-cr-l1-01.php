81113893bfae900e072811e562679419495ada54
FabianOrtizJM <garfiun18@gmail.com>
2022-10-20 16:56:02 -0500

    planeacion para hojas de chequeo

---
+++ app/Models/Inicio7-setcs-ele-cr-l1-01.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inicio7-setcs-ele-cr-l1-01 extends Model
+{
+    use HasFactory;
+}
